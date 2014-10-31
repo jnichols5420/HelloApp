27330c30bd62cbcb89dc14935c7a576646f2f9f4
jnichols5420 <jnichols@icstars.org>
2014-10-31 16:35:46 -0500

    FOR ARRAY

--- app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -1,22 +1,39 @@
 package com.teamtreehouse.funfacts;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import java.util.Random;
 
 
-public class FunFactsActivity extends Activity {
-    /** Called when the activity is first created. */
+public class FunFactsActivity extends Activity implements DialogInterface.OnClickListener {
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        // button click listeners
+        Button response = (Button) findViewById(R.id.btnResponse);
+        Button cancel = (Button) findViewById(R.id.btnCancel);
 
+
+    response.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    });
+}
+
+    /** Called when the activity is first created. */
     private String[] mTestArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
 
         // Create an ArrayAdapter that will contain all list items
 //        ArrayAdapter<String> adapter;
@@ -39,4 +56,5 @@ public class FunFactsActivity extends Activity {
 
         String randomStr = mTestArray[new Random().nextInt(mTestArray.length)];
         }
+
 }
