bd73132cfafb309d7207cde0c1e0ad83674228a4
jnichols5420 <jnichols@icstars.org>
2014-11-15 11:34:55 -0600

    updated strings, added text view for response, updated text view,  added log print, unit testing example (thanks Kelton), Log print example when on create, On button click "response" (thanks Andrew), button click listeners for response and cancel

--- app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -1,8 +1,8 @@
 package com.teamtreehouse.funfacts;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -11,65 +11,69 @@ import android.widget.TextView;
 import java.util.Random;
 
 
-public class FunFactsActivity extends Activity implements DialogInterface.OnClickListener {
-    @Override
-    public void onClick(DialogInterface dialog, int which) {
-        // button click listeners
-        final Button response = (Button) findViewById(R.id.btnResponse);
-        Button cancel = (Button) findViewById(R.id.btnCancel);
-        //Text View
-        TextView fact = (TextView) findViewById(R.id.responseView);
+public class FunFactsActivity extends Activity {
 
-
-    response.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            //button "response" clicked
-
-        }
-    });
-
-    cancel.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //button "cancel" clicked
-                mTestArray =  getResources().getStringArray(R.array.random_fact_array);
-                String r = mTestArray[rgenerator.nextInt(mTestArray.length)];
-                response.setText(r);
-            }
-        });
-}
 
     /** Called when the activity is first created. */
     private String[] mTestArray;
     private static final Random rgenerator = new Random();
 
+
+
+    public int sum(int x, int y) {
+        return x + y;
+    }
+    //Unit Testing Example
+    public void test() {
+        myassert(sum(4, 7) == 11, "4 + 7 must equal 11");
+        myassert(sum(4, -7) == -3, "4 + 7 must equal 11");
+        myassert(sum(100, -7) == -93, "4 + 7 must equal 11");
+    }
+    //
+    public void myassert(boolean isCorrect, String msg) {
+       if (isCorrect) {
+
+       }
+       else {
+            throw new Exception(msg);
+       }
+    }
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("TEST", "onCreate called");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
 
+        // button click listeners
+        final Button response = (Button) findViewById(R.id.btnResponse);
+        final Button cancel = (Button) findViewById(R.id.btnCancel);
+        //Text View
+        final TextView fact = (TextView) findViewById(R.id.responseView);
 
-        // Create an ArrayAdapter that will contain all list items
-//        ArrayAdapter<String> adapter;
+        cancel.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //button "response" clicked
+                Log.d("TEST", "Button Clicked");
+            }
+        });
 
-       // mTestArray = getResources().getStringArray(R.array.random_fact_array);
+        response.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //button "cancel" clicked
+                mTestArray =  getResources().getStringArray(R.array.random_fact_array);
+                String r = mTestArray[rgenerator.nextInt(mTestArray.length)];
+                fact.setText(r);
+            }
+        });
 
-    /* Assign the name array to that adapter and
-    also choose a simple layout for the list items */
-//        adapter = new ArrayAdapter<String>(
-//                this,
-//                android.R.layout.simple_list_item_1,
-//                mTestArray);
-
-        // Assign the adapter to this ListActivity
-//        setListAdapter(adapter);
-    }
-
-        public void getRand() {
+    }}
 
 
-        String randomStr = mTestArray[new Random().nextInt(mTestArray.length)];
-        }
-
-}
